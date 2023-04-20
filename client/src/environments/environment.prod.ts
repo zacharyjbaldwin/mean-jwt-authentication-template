83e20a906f5cfd7b25f7858b3f7851f2f33400ed
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2023-04-20 16:25:53 -0500

    removed any reference to email functionality -- this will come later

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: null,
-  doEmailVerificaiton: false,
-  doForgotPasswordReset: false
+  apiUrl: null
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,9 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  doEmailVerificaiton: false,
-  doForgotPasswordReset: false
+  apiUrl: 'http://localhost:3000/api'
 };
 
 /*
